35fd68449e4c3ade111cbbb84e0179f3720dd5c9
keithinui <55673645+keithinui@users.noreply.github.com>
2026-08-12 17:07:47 +0900

    Update key.js

--- key.js
+++ key.js
@@ -1,2 +1,2 @@
-const appId = 'e50efb62-c752-4d10-8d8b-b254a6b164d1'
-const secretKey = 'ZqYtMvRJyEsxQR3glttoA7FLnmHDcN4ZeASkHkkaKJU='
+var appId = 'e50efb62-c752-4d10-8d8b-b254a6b164d1'
+var secretKey = 'ZqYtMvRJyEsxQR3glttoA7FLnmHDcN4ZeASkHkkaKJU='
